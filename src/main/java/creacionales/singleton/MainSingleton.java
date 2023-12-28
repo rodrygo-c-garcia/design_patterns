4504716df41b3e7b27e24597a96cd33f460e0152
Rodrigo <colqueariel023@gmail.com>
2023-12-28 10:54:32 -0400

    feat: instance Conexion

--- src/main/java/creacionales/singleton/MainSingleton.java
+++ src/main/java/creacionales/singleton/MainSingleton.java
@@ -2,6 +2,13 @@ package creacionales.singleton;
 
 public class MainSingleton {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // como el metodo es estatico pertenece a la clase
+        Conexion conexion = Conexion.getInstancia();
+        conexion.conectar();
+        conexion.desconectar();
+
+        // verificamos que la instancia sea unica
+        boolean rpta = conexion instanceof Conexion;
+        System.out.println(rpta);
     }
 }
