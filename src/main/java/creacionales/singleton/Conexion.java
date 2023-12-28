74bea450fef91420fbd92c7be4e641eebeb2fee6
Rodrigo <colqueariel023@gmail.com>
2023-12-28 10:41:53 -0400

    feat: add structure singleton

--- src/main/java/creacionales/singleton/Conexion.java
+++ src/main/java/creacionales/singleton/Conexion.java
@@ -1,5 +1,24 @@
 package creacionales.singleton;
 
 public class Conexion {
+    private static Conexion instancia;
 
+    private Conexion() {
+    }
+
+    // creamos el metodo static para obtener la instancia unica
+    public static Conexion getInstancia() {
+        if (instancia == null) {
+            instancia = new Conexion();
+        }
+        return instancia;
+    }
+
+    public void conectar() {
+        System.out.println("Conectando a la base de datos");
+    }
+
+    public void desconectar() {
+        System.out.println("Desconectando a la base de datos");
+    }
 }
