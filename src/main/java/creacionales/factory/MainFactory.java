cc870b1bf2da4f333a55ae18bf3d250839bdafb8
Rodrigo <colqueariel023@gmail.com>
2023-12-28 11:52:01 -0400

    feat: call of the metod Fabrica

---
+++ src/main/java/creacionales/factory/MainFactory.java
@@ -0,0 +1,18 @@
+package creacionales.factory;
+
+public class MainFactory {
+    public static void main(String[] args) {
+        ConexionFabrica fabrica = new ConexionFabrica();
+        IConexion conexion1 = fabrica.getConexion("ORACLE");
+        conexion1.conectar();
+        conexion1.desconectar();
+
+        IConexion conexion2 = fabrica.getConexion("MYSQL");
+        conexion2.conectar();
+        conexion2.desconectar();
+
+        IConexion conexion3 = fabrica.getConexion("POSTGRE");
+        conexion3.conectar();
+        conexion3.desconectar();
+    }
+}
