f1f76f1299e2b5bb6cc2024a2fc46d6ad2513286
Rodrigo <colqueariel023@gmail.com>
2024-01-02 18:19:02 -0400

    feat: instantiate factories

--- src/main/java/creacionales/abstract_factory/MainFactory.java
+++ src/main/java/creacionales/abstract_factory/MainFactory.java
@@ -7,12 +7,12 @@ import creacionales.abstract_factory.factoryREST.IConexionREST;
 
 public class MainFactory {
     public static void main(String[] args) {
-        IFabricaAbstracta fabricaBD = new ConexionFabricaBD();
+        IFabricaAbstracta fabricaBD = FabricaProductor.getFactory("BD");
         IConexionBD conexionBD1 = fabricaBD.getBD("MYSQL");
         conexionBD1.conectar();
         conexionBD1.desconectar();
 
-        IFabricaAbstracta fabricaREST = new ConexionFabricaREST();
+        IFabricaAbstracta fabricaREST = FabricaProductor.getFactory("REST");
         IConexionREST conexionREST1 = fabricaREST.getREST("COMPRAS");
         conexionREST1.leerURL("https://www.youtube.com/watch?v=9jI-z9QN6g8");
     }
