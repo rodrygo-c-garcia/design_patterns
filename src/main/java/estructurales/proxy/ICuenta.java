b40da3fab0a3c03cb49c5162a8fd5ab741d3323a
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2024-01-18 15:40:09 -0400

    feat: add interface Icuenta

---
+++ src/main/java/estructurales/proxy/ICuenta.java
@@ -0,0 +1,7 @@
+package estructurales.proxy;
+
+public interface ICuenta {
+    Cuenta retirarDinero(Cuenta cuenta, double monto);
+    Cuenta depositarDinero(Cuenta cuenta, double monto);
+    void mostrarSaldo(Cuenta cuenta);
+}
