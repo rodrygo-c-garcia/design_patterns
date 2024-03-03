38da5e6b5dd9f60d0e3994ec7170a0701cb1a10d
colqueariel023@gmail.com <colqueariel023@gmail.com>
2024-03-03 16:33:54 -0400

    refactor: comment duplicate line

--- src/main/java/estructurales/facade/Main.java
+++ src/main/java/estructurales/facade/Main.java
@@ -5,7 +5,7 @@ public class Main {
         CheckFacade cliente1 = new CheckFacade();
         cliente1.buscar("01/01/2020", "10/01/2020", "Buenos Aires", "Madrid");
 
-        CheckFacade cliente2 = new CheckFacade();
-        cliente2.buscar("01/01/2020", "10/01/2020", "Lima", "Quito");
+        //CheckFacade cliente2 = new CheckFacade();
+        //cliente2.buscar("01/01/2020", "10/01/2020", "Lima", "Quito");
     }
 }
