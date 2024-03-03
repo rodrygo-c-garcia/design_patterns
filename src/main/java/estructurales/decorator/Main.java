a5c09548370e0b1c5e8c8777b1866504f2564596
colqueariel023@gmail.com <colqueariel023@gmail.com>
2024-03-03 16:56:11 -0400

    feat: add new instance

--- src/main/java/estructurales/decorator/Main.java
+++ src/main/java/estructurales/decorator/Main.java
@@ -4,8 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Cuenta cliente1 = new Cuenta(1, "Juan Perez");
         ICuentaBancaria cuenta = new CuentaAhorro();
-        ICuentaBancaria cuentaBlindada = new BlindajeDecorator(cuenta);
+        cuenta.abrirCuenta(cliente1);
 
-        cuentaBlindada.abrirCuenta(cliente1);
+        Cuenta cliente2 = new Cuenta(2, "Maria Lopez");
+        ICuentaBancaria cuenta2 = new CuentaCorriente();
+        // a esta cuenta agregamos un blindaje
+        ICuentaBancaria cuentaBlindada = new BlindajeDecorator(cuenta2);
+
+        cuentaBlindada.abrirCuenta(cliente2);
     }
 }
