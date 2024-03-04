4f780d91d128526b41f1bf24e0d86cb4fbb544d4
Rodrigo CG <colqueariel023@gmail.com>
2024-03-04 18:25:22 -0400

    feat: add class implements PersonaDAO

---
+++ src/main/java/design/dao/PersonaDAOImplements.java
@@ -0,0 +1,43 @@
+package design.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonaDAOImplements implements PersonaDao{
+    @Override
+    public List<Persona> listar() {
+        List<Persona> lista = new ArrayList<>();
+        Persona persona1 = new Persona();
+        persona1.setId(1);
+        persona1.setNombres("Juan");
+
+        lista.add(persona1);
+        Persona persona2 = new Persona();
+        persona2.setId(1);
+        persona2.setNombres("Maria");
+
+        lista.add(persona2);
+        return lista;
+    }
+
+    @Override
+    public Persona leerPorId(int id) {
+        System.out.println("Persona encontrada con id: " + id);
+        return null;
+    }
+
+    @Override
+    public void registrar(Persona persona) {
+        System.out.println("Persona registrada: " + persona.getNombres());
+    }
+
+    @Override
+    public void actualizar(Persona persona) {
+        System.out.println("Persona actualizada: " + persona.getNombres());
+    }
+
+    @Override
+    public void eliminar(int id) {
+        System.out.println("Persona eliminada con id: " + id);
+    }
+}
