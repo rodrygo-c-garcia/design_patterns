60de2e6a10a312c5c7476ad44d902cfa0185cb4e
Rodrigo CG <colqueariel023@gmail.com>
2024-03-04 18:24:17 -0400

    feat: add class Medico

---
+++ src/main/java/design/dao/Medico.java
@@ -0,0 +1,11 @@
+package design.dao;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter
+public class Medico {
+    private int id;
+    private String nombres;
+    private String cargo;
+}
