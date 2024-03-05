8efc4e124f0c748196447f1e607cde3a38d535b9
Rodrigo CG <colqueariel023@gmail.com>
2024-03-05 10:30:32 -0400

    feat: add IConexion instance

--- src/main/java/design/dao/Main.java
+++ src/main/java/design/dao/Main.java
@@ -2,7 +2,12 @@ package design.dao;
 
 public class Main {
     public static void main(String[] args) {
+        IConexion connection1 = new ConexionMySQL("Rodrigo", "123", "localhost");
+        IConexion connection2 = new ConexionOracle();
+
+
         CRUD<Medico> dao = new MedicoImplements();
+        dao.setConexion(connection2);
         // casting
         ((MedicoImplements) dao).curar();
 
